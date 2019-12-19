63995cebffc22d046a2a2b490bb6badb266d41f9
MaryaBat <mbat2005@mail.ru>
2019-12-19 18:29:08 +0300

    feat: add _interface.sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-const {src, dest, watch} = require ('gulp');
-const browserSync = require ('browser-sync'). create ();
-const sass = require ('gulp-sass');
+const {src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 
 // Static server
 function bs() {
-  serveSass ();
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
   watch("./*.html").on('change', browserSync.reload);
-  watch("./sass/**/.*sass", serveSass);
+  watch("./sass/**/*.sass", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
